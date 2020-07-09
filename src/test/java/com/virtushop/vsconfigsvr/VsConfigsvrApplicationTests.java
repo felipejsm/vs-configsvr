935cad78b747dec49f81f0ac7e3993e0de66927d
felipejsm <felipejsm41@gmail.com>
2020-07-09 12:23:22 -0300

    initial commit

---
+++ src/test/java/com/virtushop/vsconfigsvr/VsConfigsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtushop.vsconfigsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsConfigsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
